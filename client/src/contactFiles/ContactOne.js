6dcd200c7f8a9d926edfbcd350d3736e673d3ad3
tamerlan ermanbet uulu <tommy-eduardo@tamerlans-MacBook-Pro.local>
2020-03-15 16:08:13 -0400

    placed links

--- client/src/contactFiles/ContactOne.js
+++ client/src/contactFiles/ContactOne.js
@@ -12,8 +12,12 @@ export default function ContactOne() {
 
       <div className="google-yelp">
         <p>google us</p>
-        <p><img src={google} /></p>
-        <p><img src={yelp} /></p>
+        <a href="https://www.google.com/maps/place/The+Phone+Works+%26+Co+:+iPhone+Screen,+iPad+Screen+%26+Samsung+Screen+Repair+Brooklyn/@40.7177634,-73.9610916,17z/data=!3m1!4b1!4m5!3m4!1s0x89c2595df050dd17:0xd432190d44f538!8m2!3d40.7177634!4d-73.9589029?hl=en">
+          <p><img src={google} /></p>
+        </a>
+        <a href="https://www.yelp.com/biz/the-phone-works-and-co-brooklyn?osq=the+phone+works+and+co">
+          <p><img src={yelp} /></p>
+        </a>
         <p>we are on yelp</p>
       </div>
     </div>
--- client/src/homeFiles/Footer.js
+++ client/src/homeFiles/Footer.js
@@ -12,20 +12,28 @@ export default function Footer() {
       <div className="footer-wrapper">
         <div className="footerone">
           <div className="icon-wrapper">
-            <img src={facebook} alt="facebook" />
-            <p>facebook</p>
+            <a href="https://www.facebook.com/thephoneworksandcompany">
+              <img src={facebook} alt="facebook" />
+              <p>facebook</p>
+            </a>
           </div>
           <div className="icon-wrapper">
-            <img src={instagram} alt="instagram" />
-            <p>instagram</p>
+            <a href="https://www.instagram.com/thephoneworksandco">
+              <img src={instagram} alt="instagram" />
+              <p>instagram</p>
+            </a>
           </div>
           <div className="icon-wrapper">
-            <img src={google} alt="google" />
-            <p>google map</p>
+            <a href="https://www.google.com/maps/place/The+Phone+Works+%26+Co+:+iPhone+Screen,+iPad+Screen+%26+Samsung+Screen+Repair+Brooklyn/@40.7177634,-73.9610916,17z/data=!3m1!4b1!4m5!3m4!1s0x89c2595df050dd17:0xd432190d44f538!8m2!3d40.7177634!4d-73.9589029?hl=en">
+              <img src={google} alt="google" />
+              <p>google map</p>
+            </a>
           </div>
           <div className="icon-wrapper">
-            <img src={yelp} alt="yelp" />
-            <p>yelp</p>
+            <a href="https://www.yelp.com/biz/the-phone-works-and-co-brooklyn?osq=the+phone+works+and+co">
+              <img src={yelp} alt="yelp" />
+              <p>yelp</p>
+            </a>
           </div>
         </div>
         <div className="footertwo">
